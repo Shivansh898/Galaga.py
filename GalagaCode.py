183b860e0e0186ec36ec87757dd9697052a51952
Shivansh898 <2311374@wgsb.co.uk>
2026-09-22 19:01:55 +0100

    Add files via upload

--- GalagaCode.py
+++ GalagaCode.py
@@ -3,17 +3,29 @@ import random
 WIDTH = 800
 HEIGHT = 525
 Galaga = Actor("galaga")
-Bug = Actor("bug")
 BulletList = []
+BugList = []
 Galaga.x = 400
 Galaga.y = 475
+direction = 1 
+
+for j in range(3):
+    for i in range(6):
+        Bug = Actor("bug")
+        Bug.x =  25 + (50*i)
+        Bug.y = 25 + (45*j)
+        BugList.append(Bug)
 
 def draw():
+    
     screen.fill("blue")
     Galaga.draw()
     for b in BulletList:
         b.draw()
+    for bug in BugList:
+        bug.draw()
 def update():
+    global direction, Bug    
     if keyboard.left:
         Galaga.x -= 5
         if Galaga.x < 50:
@@ -24,6 +36,24 @@ def update():
             Galaga.x = 750
     for i in BulletList:
         i.y -= 5
+        for Bug in BugList:
+            if Bug.colliderect(i):
+                BugList.remove(Bug)
+                BulletList.remove(i)
+    if Bug in BugList:
+        for Bug in BugList:
+            Bug.x += 3*direction
+        if BugList[-1].x == 800:
+            direction = -1
+            for Bug in BugList:
+                Bug.y += 20
+                
+        elif BugList[0].x <= 10:
+            direction = 1
+            for Bug in BugList:
+                Bug.y +=20
+    elif Bug not in BugList:
+        screen.fill("black")
 
 def on_key_down(key):
     if key == keys.SPACE:
